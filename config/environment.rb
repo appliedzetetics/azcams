0b8fed704e850b319218f5a952e9188a8e424a5a
appliedzetetics <stephen@appliedzetetics.co.uk>
2013-07-01 09:52:49 +0100

    Corrected location of APP_VERSION in environment.rb file

--- config/environment.rb
+++ config/environment.rb
@@ -4,7 +4,8 @@ require File.expand_path('../application', __FILE__)
 Date::DATE_FORMATS.merge!(:default => "%d/%m/%Y")
 Time::DATE_FORMATS.merge!(:default => "%H:%M")
 
-APP_VERSION = `git describe --always` unless defined? APP_VERSION
 
 # Initialize the rails application
 Pccn::Application.initialize!
+
+APP_VERSION = `git describe --always` unless defined? APP_VERSION
