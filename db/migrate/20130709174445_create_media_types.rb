7622eb8a4d004a2cae07d286b552493330f0dbcf
appliedzetetics <stephen@appliedzetetics.co.uk>
2013-07-20 12:17:53 +0100

    Corrected mediatypes creation migration

--- db/migrate/20130709174445_create_media_types.rb
+++ db/migrate/20130709174445_create_media_types.rb
@@ -3,12 +3,7 @@ class CreateMediaTypes < ActiveRecord::Migration
     create_table :media_types do |t|
       t.integer :account_id, :null => false
       t.string :name
-
       t.timestamps
     end
-    MediaTypes.create(:name => 'Headed', :account_id => 1)
-    MediaTypes.create(:name => 'Plain', :account_id => 1)
-    MediaTypes.create(:name => 'Headed', :account_id => 0)
-    MediaTypes.create(:name => 'Plain', :account_id => 0)
   end
 end
